abf856f034c94213bf1a4f61c92125d3e79b13b7
Arne Lapõnin <arne.laponin@gmail.com>
2018-01-15 10:39:56 +0100

    modified tests

--- test/CircuitTests.java
+++ test/CircuitTests.java
@@ -127,8 +127,8 @@ public class CircuitTests {
         assertEquals(0.0, result, 0.0);
         
         HashMap<String, Double> map1 = new HashMap<String, Double>();
-        map.put("X1", 1.0);
-        map.put("X2", 1.0);
+        map1.put("X1", 1.0);
+        map1.put("X2", 1.0);
         Double result1;
         result1 = c.getResultDouble(map1);
         assertEquals(1.0, result1, 0.0);
@@ -140,13 +140,13 @@ public class CircuitTests {
         c.getInput("X1");
         c.getInput("X2");
         c.getInput("and");
-        c.getInput("X3");
+        c.getInput("X1");
+        c.getInput("negation");
         c.getInput("or");
         
         HashMap<String, Double> map1 = new HashMap<String, Double>();
         map1.put("X1", 0.5);
         map1.put("X2", 0.5);
-        map1.put("X3", 0.5);
         
         Double result;
         result = c.getResultDouble(map1);
@@ -155,10 +155,16 @@ public class CircuitTests {
         HashMap<String, Double> map = new HashMap<String, Double>();
         map.put("X1", 0.0);
         map.put("X2", 1.0);
-        map.put("X3", 0.0);
         Double result1;
         result1 = c.getResultDouble(map);
-        assertEquals(1.0, result, 0.0);
+        assertEquals(1.0, result1, 0.0);
+        
+        HashMap<String, Double> map2 = new HashMap<String, Double>();
+        map2.put("X1", 0.0);
+        map2.put("X2", 2.0);
+        Double result2;
+        result2 = c.getResultDouble(map2);
+        assertEquals(1.0, result2, 0.0);
     }
     
     
